8be63507cf55d72c2a06747fd4b02aca165f5371
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-22 08:31:51 +0600

    create maain page and body bg

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+const TicTacToe = () => {
+  return (
+    <div className="h-screen w-full bg-slate-800 flex justify-center items-center">
+      <p className="text-sky-400 font-semibold text-3xl">Tic Tac Toe</p>
+    </div>
+  );
+};
+
+export default TicTacToe;
