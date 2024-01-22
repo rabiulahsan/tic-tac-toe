39cfe12732350ce49b43c20460ac590e6c5df03c
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-22 12:58:51 +0600

    color change

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 const TicTacToe = () => {
   return (
     <div className="h-screen w-full bg-slate-800 flex justify-center items-center flex-col">
-      <p className="text-white font-semibold text-3xl block mb-[2%]">
+      <p className="text-sky-200 font-semibold text-3xl block mb-[2%]">
         Tic Tac Toe
       </p>
       <Board></Board>
